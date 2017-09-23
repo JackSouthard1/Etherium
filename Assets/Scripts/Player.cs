a55a5fba8c6f428298e75faa626b63a10fd474b8
Nathan Jew <nathankjew@gmail.com>
2017-09-22 21:13:58 -0700

    Added inventory UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,37 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	public List<Resource> inventory = new List<Resource>();
+	public Transform inventoryParent;
+
 	Body body;
 	TerrainManager tm;
+
+	//TODO: what would be the best data structures to use here?
+	//we could simplify these to lists, and then get the ResourceInfo from the TerrainManager instead
+	Dictionary<TerrainManager.ResourceInfo, int> inventory = new Dictionary<TerrainManager.ResourceInfo, int>();
+	Dictionary<TerrainManager.ResourceInfo, UIResource> inventoryUI = new Dictionary<TerrainManager.ResourceInfo, UIResource>();
+
+	public GameObject uiResourcePrefab;
 
 	void Start () {
 		tm = GameObject.Find ("Terrain").GetComponent<TerrainManager> ();
 		body = GetComponent<Body> ();
+
+		InitInventory ();
+	}
+
+	void InitInventory () {
+		foreach (TerrainManager.ResourceInfo resourceInfo in tm.resourceInfos) {
+			inventory.Add(resourceInfo, 0);
+
+			GameObject newUIResourceObj = (GameObject)Instantiate (uiResourcePrefab, inventoryParent);
+			UIResource newUIResource = newUIResourceObj.GetComponent<UIResource> ();
+			newUIResource.Init (resourceInfo);
+
+			inventoryUI.Add(resourceInfo, newUIResource);
+		}
+
+		UpdateInventoryUI ();
 	}
 
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.I)) {
-			string str = "Inventory: ";
-			for (int i = 0; i < inventory.Count; i++) {
-				str += inventory [i].info.type + ", ";
+			string str = "Inventory - ";
+			foreach (KeyValuePair<TerrainManager.ResourceInfo, int> keyValuePair in inventory) {
+				str += keyValuePair.Key.type.ToString () + ": " + keyValuePair.Value.ToString () + ", ";
 			}
 			print (str);
-		}
-
-		if (Input.GetKeyDown (KeyCode.E)) {
-			if (inventory.Count > 0) {
-				DropResource (inventory.Count - 1);
-			}
 		}
 	}
 
 	public void CollectResource (Resource resource) {
 		int stackCount = resource.resourceGO.Count;
-		for (int i = 0; i < stackCount; i++) {
-			Resource newResource = new Resource (resource.info, null, null);
-			inventory.Add (newResource);
-		}
+		inventory [resource.info] += stackCount;
 			
 		resource.island.resources.Remove (resource);
 	
@@ -41,26 +56,40 @@ public class Player : MonoBehaviour {
 		for (int i = 0; i < stackCount; i++) {
 			Destroy (resource.resourceGO [i]);
 		}
+
+		UpdateInventoryUI ();
 	}
 
-	public void DropResource (int index) {
+	public void DropResource (TerrainManager.ResourceInfo resourceInfo) {
+		if (inventory [resourceInfo] == 0) {
+			return;
+		}
+
 		Vector2 posV2 = new Vector2 (transform.position.x, transform.position.z);
 		if (tm.GetTileAtPosition(posV2) == null) {
 			return;
 		}
-
-		TerrainManager.ResourceInfo resourceInfoToSpawn = inventory [index].info;
-
+			
 		if (tm.GetResourceAtPosition (posV2) != null) {
 			Resource curResource = tm.GetResourceAtPosition (posV2);
-			if (curResource.info.type == resourceInfoToSpawn.type) {
-				tm.SpawnResource (transform.position, resourceInfoToSpawn, body.location);
-				inventory.RemoveAt (index);
+			if (curResource.info.type != resourceInfo.type) {
+				return;
 			}
-		} else {
-//			print((tm.SpawnResource (transform.position, resourceInfoToSpawn, tm.transform, body.location)).island);
-			tm.SpawnResource (transform.position, resourceInfoToSpawn, body.location);
-			inventory.RemoveAt (index);
+		}
+
+		tm.SpawnResource (transform.position, resourceInfo, body.location);
+		inventory[resourceInfo]--;
+		UpdateInventoryUI ();
+	}
+
+	void UpdateInventoryUI () {
+		float leftAnchor = 0f;
+		foreach (KeyValuePair<TerrainManager.ResourceInfo, UIResource> keyValuePair in inventoryUI) {
+			keyValuePair.Value.amount = inventory[keyValuePair.Key];
+			keyValuePair.Value.UpdateVisual();
+
+			keyValuePair.Value.gameObject.GetComponent<RectTransform> ().offsetMin = new Vector2 (leftAnchor, 0f);
+			leftAnchor += keyValuePair.Value.totalWidth;
 		}
 	}
 }
---
+++ Assets/Scripts/UIResource.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIResource : MonoBehaviour
+{
+	Text amountText;
+	Button resourceButton;
+
+	public int amount;
+
+	public float totalWidth {
+		get {
+			if (amount > 0)
+				return amountText.rectTransform.localPosition.x + amountText.rectTransform.rect.width;
+			else
+				return 0f;
+		}
+	}
+
+	void Awake () {
+		amountText = gameObject.GetComponentInChildren<Text> ();
+		resourceButton = gameObject.GetComponentInChildren<Button> ();
+	}
+
+	public void Init(TerrainManager.ResourceInfo resourceInfo) {
+		resourceButton.image.sprite = resourceInfo.sprite;
+		resourceButton.onClick.AddListener(() => GameObject.FindObjectOfType<Player>().DropResource(resourceInfo));
+	}
+
+	public void UpdateVisual() {
+		if (amount > 0) {
+			float textWidth = (float)(amount.ToString ().Length * amountText.fontSize);
+			amountText.rectTransform.sizeDelta = new Vector2 (textWidth, amountText.rectTransform.rect.height);
+			amountText.text = amount.ToString ();
+
+			SetVisible (true);
+		} else {
+			SetVisible (false);
+		}
+	}
+
+	void SetVisible(bool isVisible) {
+		amountText.enabled = isVisible;
+
+		resourceButton.enabled = isVisible;
+		resourceButton.image.enabled = isVisible;
+	}
+}
